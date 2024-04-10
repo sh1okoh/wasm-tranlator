ca04be93467c26f3b4da09aa185d35e7356d57d3
sh1okoh <kooooo.0719task@gmail.com>
2024-04-10 21:01:50 +0900

    feat: 本実装

--- translator.c
+++ translator.c
@@ -3,7 +3,8 @@
 
 FILE *ifp, *ofp;
 int main ( int argc, char *argv[] ) {
-  unsigned char c;
+  unsigned char c, sec;
+  int sec_siz;
 
   if ( argc != 3 ) {
     printf("Error: translator <input file> <output file>\n");
@@ -20,8 +21,38 @@ int main ( int argc, char *argv[] ) {
     exit(0);
   }
 
+  for (sec_siz = 8; sec_siz > 0; sec_siz-- ) {
+    fread(&c, sizeof(char), 1, ifp);
+  }
+
+
   while ( fread(&c, sizeof(char), 1, ifp ) == 1) {
-    fprintf( ofp, "\"%02X\",\n", c);
+    sec = c;
+    fread( &c, sizeof(char), 1, ifp);
+    sec_siz = c & 0x7F;
+    if ((c & 0x80) != 0) {
+      fread( &c, sizeof(char), 1, ifp);
+      sec_siz = (((int)c & 0x7F) << 7) | sec_siz;
+      if ((c & 0x80) != 0) {
+        fread( &c, sizeof(char), 1, ifp);
+        sec_siz = (((int)c & 0x7F) << 14) | sec_siz;
+        if ((c & 0x80) != 0) {
+          fread( &c, sizeof(char), 1, ifp);
+          sec_siz = (((int)c & 0x7F) << 21) | sec_siz;
+        }
+      }
+    }
+    if ( sec != 0x0A) {
+      for (; sec_siz > 0; sec_siz-- ) {
+        fread(&c, sizeof(char), 1, ifp);
+      }
+    } else { // Code section
+      fread(&c, sizeof(char), 1, ifp);
+      for (; sec_siz > 0; sec_siz-- ) {
+        fread(&c, sizeof(char), 1, ifp);
+        fprintf( ofp, "\"%02X\",\n", c);
+      }
+    }
   }
 
   fclose( ifp );
